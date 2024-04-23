554cc6ee6ee9069d66eeda26bee579a3056a949a
phamducphuongminh <phamducphuongminh0103@gmail.com>
2024-04-23 15:11:57 +0700

    Connect and Close mongodb

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,8 +1,7 @@
 import 'dotenv/config'
 
 const env = {
-  USERNAME_MONGODB: process.env.USERNAME_MONGODB,
-  PASSWORD_MONGODB: process.env.PASSWORD_MONGODB,
+  MONGODB_URI: process.env.MONGODB_URI,
   DATABASE_NAME_MONGODB: process.env.DATABASE_NAME_MONGODB,
   APP_HOST: process.env.APP_HOST,
   APP_PORT: process.env.APP_PORT
--- src/server.js
+++ src/server.js
@@ -1,12 +1,31 @@
 import express from 'express'
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import exitHook from 'async-exit-hook'
+import 'dotenv/config'
 import env from '~/config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.send('Hello World!')
+  })
 
-app.listen(env.APP_PORT, env.APP_HOST, () => {
-  console.log(`Example app listening on port ${env.APP_PORT}`)
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Example app listening on port ${env.APP_PORT}`)
+  })
+
+  exitHook(() => {
+    console.log('Disconnected to Mongodb Cloud Atlas!')
+    CLOSE_DB()
+  })
+}
+
+CONNECT_DB()
+  .then(() => console.log('Connected to Mongodb Cloud Atlas!'))
+  .then(() => START_SERVER())
+  .catch((err) => {
+    console.log(err)
+    process.exit(0)
+  })
